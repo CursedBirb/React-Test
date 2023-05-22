16ad5fc0fcf3df13a210f8f357beed5d56b80dc9
CursedBirb <47064429+CursedBirb@users.noreply.github.com>
2023-05-22 18:13:38 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <input type="text" name="argument1" value="1" />
+      <br></br>
+      <input type="text" name="argument2" value="1" />
+      <br></br>
+      <button /*onClick*/>Oblicz</button>
+    </div>
+  );
+}
